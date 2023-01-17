4baf8c2168c8f43b9cdf23156c1dc5f7c5c9939a
first-second <firstsecondis72@gmail.com>
2023-01-17 15:58:08 +0530

    allowed hosts

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.109','www.sdshp.in','sdshp.in','127.0.0.1','0.0.0.0']
+ALLOWED_HOSTS = ['www.sdshp.in']
 
 
 # Application definition
